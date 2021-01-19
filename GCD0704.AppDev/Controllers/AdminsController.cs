591c75c080a52c58f76acda1501e62553be3f75d
Vinh Hoang Nhu (FE FGW DN) <VinhHN3@fe.edu.vn>
2021-01-19 17:06:27 +0700

    Implement Profiles action of AdminsController

--- GCD0704.AppDev/Controllers/AdminsController.cs
+++ GCD0704.AppDev/Controllers/AdminsController.cs
@@ -1,5 +1,8 @@
 ﻿using GCD0704.AppDev.Models;
-using System.Data.Entity;
+using GCD0704.AppDev.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -9,17 +12,34 @@ namespace GCD0704.AppDev.Controllers
 	public class AdminsController : Controller
 	{
 		private ApplicationDbContext _context;
+		private UserManager<ApplicationUser> _userManager;
 		public AdminsController()
 		{
 			_context = new ApplicationDbContext();
+			_userManager = new UserManager<ApplicationUser>(
+				new UserStore<ApplicationUser>(new ApplicationDbContext())
+			);
+
 		}
 		// GET: Admins
 		public ActionResult Profiles()
 		{
 			var users = _context.Users
-				.Include(u => u.Roles)
 				.ToList();
-			return View(users);
+
+			List<UserWithRoleViewModel> usersWithRole = new List<UserWithRoleViewModel>();
+
+			foreach (var user in users)
+			{
+				usersWithRole.Add(new UserWithRoleViewModel()
+				{
+					UserName = user.UserName,
+					RoleName = _userManager.GetRoles(user.Id)[0]
+				});
+			}
+
+
+			return View(usersWithRole);
 		}
 	}
 }
---
+++ GCD0704.AppDev/ViewModels/UserWithRoleViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GCD0704.AppDev.ViewModels
+{
+	public class UserWithRoleViewModel
+	{
+		public string UserName { get; set; }
+		public string RoleName { get; set; }
+	}
+}
